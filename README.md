9fc41486129b898466c8525a91a0f6c74b6b8156
Finn# <105933273+finn-sharp@users.noreply.github.com>
2024-07-18 17:38:45 +0900

    first commit
    initial files

---
+++ README.md
@@ -0,0 +1,2 @@
+## github - deploy method 
+[link](https://docs.github.com/ko/pages/getting-started-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site)
---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+function openPopup(title, content) {
+    document.getElementById('popup-title').textContent = title;
+    document.getElementById('popup-content').textContent = content;
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function navigateTo(page) {
+    if (page=='home') {
+        let url = '../index.html'
+        window.location.href = url;
+    } else {
+        let url = '/code/'+ page + '.html';
+        window.location.href = url;
+    }
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // 이미지가 넘어가는 시간 (3초)
+}
